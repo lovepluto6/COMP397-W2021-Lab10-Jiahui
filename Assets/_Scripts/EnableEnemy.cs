23ee5c6de50fde51de3a231ef499c81d7c98ba4d
pluto <luanrenfeng@gmail.com>
2021-03-27 19:08:11 -0400

    lab 10

---
+++ Assets/_Scripts/EnableEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnableEnemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public float delay = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnableNavMeshAgent());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //coroutine to wait before enabling AI
+    IEnumerator EnableNavMeshAgent()
+    {
+        yield return new WaitForSeconds(delay);
+        agent.enabled = true;
+        StopCoroutine(EnableNavMeshAgent());
+    }
+}
